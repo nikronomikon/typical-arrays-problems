ee0fef4600a4f3a7714f18ed7c7fbcad11f91e6a
Nikita Ivanchikov <niva.pho@gmail.com>
2020-09-30 21:09:02 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let min = array.filter((a) => typeof(a) === 'number').sort((a,b) => a-b);
+  return min.shift();
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let max = array.filter((a) => typeof(a) === 'number').sort((a,b) => a-b);
+  return max.pop();
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let arrNum = array.filter((a) => typeof(a) === 'number'),
+  avg = arrNum.reduce((a,b) => a + b) / arrNum.length;
+  return avg;
 }
